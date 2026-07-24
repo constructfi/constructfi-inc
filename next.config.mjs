7400a1246ec1ab7e5d0f6c22b22e78c9e71aac7a
ConstructFi Build <build@constructfi.co>
2026-07-24 04:58:13 +0000

    Enable static export (output: export) for preview deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   images: { unoptimized: true },
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
